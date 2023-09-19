8caa68d5265a826be651c6fcfe85d6ad3581db48
sandra <paniaguagonzalezsandra@gmail.com>
2023-09-19 09:56:27 +0200

    Eliminados dos archivos de test

--- text_exitoso.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-
-class Test(unittest.TestCase):
-    def test_run(self):
-        expected = 42
-        actual = 42
-        self.assertEqual(expected, actual)
--- text_fallido.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-
-class Test(unittest.TestCase):
-    def test_run(self):
-        expected = 42
-        actual = 314
-        self.assertEqual(expected, actual)
